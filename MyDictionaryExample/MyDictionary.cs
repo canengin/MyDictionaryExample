cb93c33eca804b7d486faa302735468ce498109f
Can Engin <kcanengin@gmail.com>
2021-01-23 19:24:49 +0300

    Add project files.

---
+++ MyDictionaryExample/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryExample
+{
+    public class MyDictionary<T>
+    {
+        T[] members;
+        public MyDictionary()
+        {
+            members = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] tempArray = members;
+            members = new T[members.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                members[i] = tempArray[i];
+
+            }
+            members[members.Length - 1] = item;
+        }
+        public int Length
+        {
+            get { return members.Length; }
+        }
+        public T[] Items
+        {
+            get { return members; }
+        }
+
+    }
+}
---
+++ MyDictionaryExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyDictionaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> Uyeler = new MyDictionary<string>();
+
+            Uyeler.Add("Kütüphane Kulübü");
+            Console.WriteLine(Uyeler.Length);
+            Uyeler.Add("Satranç Kulübü");
+            Console.WriteLine(Uyeler.Length);
+            Uyeler.Add("Spor Kulübü");
+            Console.WriteLine(Uyeler.Length);
+            Uyeler.Add("Sağlık Kulübü");
+            Console.WriteLine(Uyeler.Length);
+            Uyeler.Add("Girişimcilik Kulübü");
+            Console.WriteLine(Uyeler.Length);
+
+            foreach (var uye in Uyeler.Items)
+            {
+                Console.WriteLine(uye);
+            }
+            Console.ReadLine();
+        }
+    }
+}
